fdf3dc0455941ec9c51647e850f6b74ceb7dd070
Jaymin Gohil <121221395+JayminGohil@users.noreply.github.com>
2024-09-05 22:07:39 +0530

    Initial Commit
    ....

---
+++ 202101619010020/src/main/java/gls/updateUser.java
@@ -0,0 +1,70 @@
+package gls;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class updateUser
+ */
+@WebServlet("/updateUser")
+public class updateUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public updateUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		try {
+			Integer uid = Integer.parseInt(request.getParameter("uid"));
+			String uname = request.getParameter("uname");
+			String upass = request.getParameter("upass");
+			String umail = request.getParameter("umail");
+			Long unum = Long.parseLong(request.getParameter("unum"));
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/jaymin","root","admin");
+			
+			PreparedStatement pst = con.prepareStatement("UPDATE users SET uname=?,upass=?,unum=?,umail=? WHERE uid=?");
+			pst.setString(1, uname);
+			pst.setString(2, upass);
+			pst.setLong(3, unum);
+			pst.setString(4, umail);
+			pst.setInt(5, uid);
+			pst.executeUpdate();
+			
+			out.println("<html><body><h1>Data Update In Table Successfully.</h1>");
+			out.println("<a href='index.html'>Back To Home</a>");
+			out.println("</body></html>");
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
